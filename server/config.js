6a5e218ddf468141fdd9e22e408a0a33ebb3e813
Alexey Kapustin <djkah11@yandex.ru>
2017-05-07 18:42:09 +0300

    Fix database url

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 module.exports = {
 
     'secret': 'ilovescotchyscotch',
-    'database': 'mongodb://noder:noderauth&54;proximus.modulusmongo.net:27017/so9pojyN'
+    'database': 'mongodb://localhost:27017/Users;'
 
 };
